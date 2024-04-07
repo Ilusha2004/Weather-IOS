92e358a51104e4c40445bad2fcd9017aa07ea902
Ilusha2004 <ilakovunov@gmail.com>
2024-04-08 02:38:54 +0300

    new Files: enums for WeatherIcons

---
+++ lib/enum/day_night.dart
@@ -0,0 +1,4 @@
+enum DayNight {
+  day,
+  night
+}
---
+++ lib/enum/weather_type_main.dart
@@ -0,0 +1,12 @@
+enum CurrentWeather {
+  clearSky,
+  fewClouds,
+  scatteredClouds,
+  brokenClouds,
+  showerRain,
+  rain,
+  thunderstorm,
+  snow,
+  mist,
+  unknown
+}
